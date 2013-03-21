40109bb938b9b61e5f7cb8fae2eb3dab1c7a078e
Sam Fu <sam@s-SatL-x.(none)>
2013-03-21 13:34:26 -0400

    A little stuff.

--- test/models/glmlTests.scala
+++ test/models/glmlTests.scala
@@ -7,11 +7,11 @@ import scalajdo.DataStore
 import scalajdo.ScalaPersistenceManager
 
 class glmlTests extends FunSuite {
-    val pm = DataStore.pm
+  val pm = DataStore.pm
     
-    test("District.getTopSchools") {
-      val cand = QDistrict.candidate
-      val d1 = pm.query[District].filter(cand.name.eq("1")).executeOption().get
-      assert(d1.getTopSchools(None) === List("test"))
-	}
+  test("District.getTopSchools") {
+    val cand = QDistrict.candidate
+    val d1 = pm.query[District].filter(cand.name.eq("1")).executeOption().get
+    assert(d1.getTopSchools(None) === List("test"))
+  }
 }
